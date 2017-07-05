f18826718eb28f781449a76ddc51333c017b5e09
hushengmeng <hushengmeng@dangdang.com>
2017-07-05 15:21:29 +0800

    提交

--- src/main/java/com/business/ucenter/controller/UserController.java
+++ src/main/java/com/business/ucenter/controller/UserController.java
@@ -3,6 +3,8 @@ package com.business.ucenter.controller;
 import com.business.base.controller.BaseController;
 import com.business.ucenter.model.User;
 import com.business.ucenter.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,11 +21,15 @@ import javax.annotation.Resource;
 @RequestMapping("login")
 public class UserController extends BaseController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Resource(name="userService")
     IUserService userService;
 
     @RequestMapping("index")
     public String loginSubmit(Model model, User user, String captcha) {
+        System.out.println("1111111111");
+        logger.error("2222222");
         User u = userService.findMasterById(1L);
         return "main";
     }
