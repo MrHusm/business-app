4930ac90c58ad5f551dd2606d50dddbfd6eed917
hushengmeng <hushengmeng@dangdang.com>
2017-07-06 18:55:40 +0800

    Rejected commit: Default

--- src/main/java/com/business/base/support/SqlSessionCallback.java
+++
@@ -1,8 +0,0 @@
-package com.business.base.support;
-
-import org.apache.ibatis.session.SqlSession;
-
-public abstract interface SqlSessionCallback
-{
-    public abstract Object doInSqlSession(SqlSession paramSqlSession);
-}
